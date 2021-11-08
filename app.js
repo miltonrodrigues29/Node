d8d671129f6648a02df4524035bcad847c014491
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2021-11-08 19:21:00 +0530

    -

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-if(req.url==='/')
-{
-    res.end("This is out home page")
-}
-if(req.url==='/about')
-{
-    res.end("This is about page")
-}
-res.end(`<h1>Oops</h1>
-<p>Error occured </p>`)
-})
-
-server.listen(3000)
+const _ = require('lodash')
+const items = [1,[2,[3,[4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
